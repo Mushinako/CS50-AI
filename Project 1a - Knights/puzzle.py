1045423f8e6b0262f830643250713e96c1f51bf2
Mushinako <ridoedee@gmail.com>
2020-10-10 09:38:12 -0700

    1a solution

--- Project 1a - Knights/puzzle.py
+++ Project 1a - Knights/puzzle.py
@@ -12,21 +12,72 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # A is knight xor knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    Or(
+        # If A honest, then A is both a knight and a knave
+        And(AKnight, And(AKnight, AKnave)),
+        # If A dishonest, then A is **not** both a knight and a knave
+        And(AKnave, Not(And(AKnight, AKnave)))
+    )
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A is knight xor knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    # B is knight xor knave
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    Or(
+        # A is honest, then both are knaves
+        And(AKnight, And(AKnave, BKnave)),
+        # A is dishonest, the not both are knaves
+        And(AKnave, Not(And(AKnave, BKnave)))
+    )
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # A is knight xor knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    # B is knight xor knave
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    Or(
+        And(
+            AKnight,    # A is honest, then both are the same kind
+            Or(
+                And(AKnight, BKnight),      # Both are knights
+                And(AKnave, BKnave)         # Both are knaves
+            )
+        ),
+        And(
+            AKnave,     # A is dishonest, then both are **not** the same kind
+            Not(Or(
+                And(AKnight, BKnight),      # (Not) both are knights
+                And(AKnave, BKnave)         # (Not) both are knaves
+            ))
+        )
+    ),
+    Or(
+        And(
+            BKnight,    # B is honest, then both are of different kinds
+            Or(
+                And(AKnight, BKnave),       # A is knight and B is knave
+                And(AKnave, BKnight)        # A is knave and B is knight
+            )
+        ),
+        And(
+            BKnave,     # B is dishonest, then both are the same kind
+            Or(
+                And(AKnight, BKnight),      # Both are knights
+                And(AKnave, BKnave)         # Both are knaves
+            )
+        )
+    )
 )
 
 # Puzzle 3
@@ -35,7 +86,36 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # A is knight xor knave
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    # B is knight xor knave
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    # C is knight xor knave
+    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))),
+    Or(
+        And(
+            BKnight,    # If B is honest, then A said "I am a knave"
+            Or(
+                And(AKnight, AKnave),   # If A is honest, then A is a knave
+                And(AKnave, AKnight)    # If A is dishonest, then A is a knight
+            )
+        ),
+        And(
+            BKnave,     # If B is dishonest, then A said "I am a knight"
+            Or(
+                And(AKnight, AKnight),  # If A is honest, then A is a knight
+                And(AKnave, AKnave)     # If A is dishonest, then A is a knave
+            )
+        )
+    ),
+    Or(
+        And(BKnight, CKnave),   # If B is honest, then C is knave
+        And(BKnave, CKnight)    # If B is dishonest, then C is knight
+    ),
+    Or(
+        And(CKnight, AKnight),  # If C is honest, then A is knight
+        And(CKnave, AKnave),    # IF C is dishonest, then A is knave
+    )
 )
 
 
